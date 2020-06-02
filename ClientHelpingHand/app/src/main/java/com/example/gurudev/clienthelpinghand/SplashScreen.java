2cf46b03799bb4f6178b808486dbf3ea9a344ad6
Yalagala <gurudev.yalagala@esko.com>
2020-06-02 22:39:04 +0530

    Code Clenup

--- ClientHelpingHand/app/src/main/java/com/example/gurudev/clienthelpinghand/SplashScreen.java
+++ ClientHelpingHand/app/src/main/java/com/example/gurudev/clienthelpinghand/SplashScreen.java
@@ -6,13 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
+
+    // Global
     private static int SPLASH_TIME_OUT = 5000;
     @Override
+    
+    // on create method
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
+
+        // Set the content on view
         setContentView(R.layout.activity_splash_screen);
+
+        // Create handler
         new Handler().postDelayed(new Runnable() {
             @Override
+            
+            // Set the intent
             public void run() {
                 Intent intent = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(intent);
